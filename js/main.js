9ecb609cefe18197122c829b985950b3d937a915
Shawn Augustine <111126991+SchwannFGXD@users.noreply.github.com>
2022-08-31 02:27:57 +0100

    added icons for interests + some js interactions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
-alert("works")
+document.getElementById("aboutme").addEventListener("mouseover", () => {
+    document.getElementById("aboutme").style.color = 'rgb(0, 162, 232)';
+
+});
+
+document.getElementById("aboutme").addEventListener("mouseleave", () => {
+    document.getElementById("aboutme").style.color = 'rgb(26, 81, 163)';
+
+});
+
+document.getElementById("personalprojects").addEventListener("mouseover", () => {
+    document.getElementById("personalprojects").style.color = 'rgb(0, 162, 232)';
+
+});
+
+document.getElementById("personalprojects").addEventListener("mouseleave", () => {
+    document.getElementById("personalprojects").style.color = 'black';
+
+});
+
+
+
+
+document.getElementById("aisection").addEventListener("mouseover", function changeAInew() {
+    document.getElementById("aisection").style.backgroundImage = 'url(logo/aihoverlogo.png)';
+
+});
+
+document.getElementById("aisection").addEventListener("mouseleave", function changeAIold() {
+    document.getElementById("aisection").style.backgroundImage = 'url(logo/ailogo.png)';
+
+});
+
+document.getElementById("gamessection").addEventListener("mouseover", function changeGamenew() {
+    document.getElementById("gamessection").style.backgroundImage = 'url(logo/gamehoverlogo.png)';
+
+});
+
+document.getElementById("gamessection").addEventListener("mouseleave", function changeGameold() {
+    document.getElementById("gamessection").style.backgroundImage = 'url(logo/gamelogo.png)';
+
+});
+document.getElementById("artsection").addEventListener("mouseover", function changeArtnew() {
+    document.getElementById("artsection").style.backgroundImage = 'url(logo/arthoverlogo.png)';
+
+});
+
+document.getElementById("artsection").addEventListener("mouseleave", function changeArtold() {
+    document.getElementById("artsection").style.backgroundImage = 'url(logo/artlogo.png)';
+
+});
