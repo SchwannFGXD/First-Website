85abe5ae9f689e89d1daa941791794c14c8dfcd7
Shawn Augustine <111126991+SchwannFGXD@users.noreply.github.com>
2022-08-31 20:08:23 +0100

    more js + projects tab semi completed

---
+++ js/project.js
@@ -0,0 +1,60 @@
+document.getElementById("aboutme").addEventListener("mouseover", () => {
+    document.getElementById("aboutme").style.color = 'rgb(0, 162, 232)';
+
+});
+
+document.getElementById("aboutme").addEventListener("mouseleave", () => {
+    document.getElementById("aboutme").style.color = 'rgb(26, 81, 163)';
+
+});
+
+document.getElementById("personalprojects").addEventListener("mouseover", () => {
+    document.getElementById("personalprojects").style.color = 'rgb(0, 162, 232)';
+
+});
+
+document.getElementById("personalprojects").addEventListener("mouseleave", () => {
+    document.getElementById("personalprojects").style.color = 'black';
+
+});
+
+
+document.getElementById("project").addEventListener("mouseover", () => {
+    document.getElementById("projecttitle").style.color = 'rgb(0, 162, 232)';
+
+});
+
+document.getElementById("project").addEventListener("mouseleave", () => {
+    document.getElementById("projecttitle").style.color = 'black';
+
+});
+
+document.getElementById("crossword").addEventListener("mouseover", () => {
+    document.getElementById("crosswordtitle").style.color = 'rgb(0, 162, 232)';
+
+});
+
+document.getElementById("crossword").addEventListener("mouseleave", () => {
+    document.getElementById("crosswordtitle").style.color = 'black';
+
+});
+
+document.getElementById("axecatcher").addEventListener("mouseover", () => {
+    document.getElementById("axecatchertitle").style.color = 'rgb(0, 162, 232)';
+
+});
+
+document.getElementById("axecatcher").addEventListener("mouseleave", () => {
+    document.getElementById("axecatchertitle").style.color = 'black';
+
+});
+
+document.getElementById("leaguedodgecalculator").addEventListener("mouseover", () => {
+    document.getElementById("leaguedodgecalculatortitle").style.color = 'rgb(0, 162, 232)';
+
+});
+
+document.getElementById("leaguedodgecalculator").addEventListener("mouseleave", () => {
+    document.getElementById("leaguedodgecalculatortitle").style.color = 'black';
+
+});
