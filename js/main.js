5af08adb5d88c8071dc86e879cd6cf6519e29ad9
Shawn Augustine <111126991+SchwannFGXD@users.noreply.github.com>
2022-09-01 03:21:43 +0100

    finished interests for now

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 document.getElementById("aboutme").addEventListener("mouseover", () => {
     document.getElementById("aboutme").style.color = 'rgb(0, 162, 232)';
 
@@ -47,5 +48,35 @@ document.getElementById("artsection").addEventListener("mouseover", function cha
 
 document.getElementById("artsection").addEventListener("mouseleave", function changeArtold() {
     document.getElementById("artsection").style.backgroundImage = 'url(logo/artlogo.png)';
+
+});
+
+
+
+
+document.getElementById("aisection").addEventListener("mousemove", function changeAiTnew() {
+    document.getElementById("aitext").style.fontSize = "20px";
+});
+
+document.getElementById("aisection").addEventListener("mouseleave", function changeAiTold() {
+    document.getElementById("aitext").style.fontSize = "0px";
+
+});
+
+document.getElementById("gamessection").addEventListener("mousemove", function changeGameTnew() {
+    document.getElementById("gametext").style.fontSize = "20px";
+});
+
+document.getElementById("gamessection").addEventListener("mouseleave", function changeGameTold() {
+    document.getElementById("gametext").style.fontSize = "0px";
+
+});
+
+document.getElementById("artsection").addEventListener("mousemove", function changeArtTnew() {
+    document.getElementById("arttext").style.fontSize = "20px";
+});
+
+document.getElementById("artsection").addEventListener("mouseleave", function changeArtTold() {
+    document.getElementById("arttext").style.fontSize = "0px";
 
 });
